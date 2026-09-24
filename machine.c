a9047a2d9561ffe0f562eaa79171cb2a0b85dc69
Sammsai <i@zcee.cc>
2026-09-25 07:42:25 +0800

    Update

--- machine.c
+++ machine.c
@@ -1126,29 +1126,21 @@ __code const struct machine machine = {
 	.min_port = 3,
 	.max_port = 8,
 	.n_sfp = 2,
-	// 对调 SFP 左右物理端口映射（物理端口 5 和 6 互换）
-	.log_to_phys_port = {0, 0, 0, 5, 1, 2, 3, 4, 6},
+	.log_to_phys_port = {0, 0, 0, 6, 1, 2, 3, 4, 5},
 	.phys_to_log_port = {4, 5, 6, 7, 3, 8, 0, 0, 0},
-	.is_sfp = {0, 0, 0, 1, 0, 0, 0, 0, 2},
+	.is_sfp = {0, 0, 0, 2, 0, 0, 0, 0, 1},
 
-	// Left SFP port (对应 SDS0，与物理端口 5 绑定)
-	// .sfp_port[0].pin_detect = GPIO37,
-	// .sfp_port[0].pin_los = GPIO_NA,
-	// .sfp_port[0].sds = 0,
-	// .sfp_port[0].i2c = I2CBUS( GPIO41_I2C_SDA3_MDIO1, GPIO40_I2C_SCL3_MDC1 ),
-    .sfp_port[0].pin_detect = GPIO37, // 或测试原有的 GPIO37/38
-    .sfp_port[0].pin_los = GPIO_NA, 
-    .sfp_port[0].sds = 0, // 调换 SDS 通道
-    .sfp_port[0].i2c = I2CBUS( GPIO39_I2C_SDA4, GPIO40_I2C_SCL3_MDC1 ),
-	// Right SFP port (对应 SDS1，与物理端口 6 绑定)
-	// .sfp_port[1].pin_detect = GPIO38,
-	// .sfp_port[1].pin_los = GPIO_NA,
-	// .sfp_port[1].sds = 1,
-	// .sfp_port[1].i2c = I2CBUS( GPIO39_I2C_SDA4, GPIO40_I2C_SCL3_MDC1 ),
-    .sfp_port[1].pin_detect = GPIO38,
-    .sfp_port[1].pin_los = GPIO_NA, 
-    .sfp_port[1].sds = 1, // 调换 SDS 通道
-    .sfp_port[1].i2c = I2CBUS( GPIO41_I2C_SDA3_MDIO1, GPIO40_I2C_SCL3_MDC1 ),
+	// Left SFP port
+	.sfp_port[0].pin_detect = GPIO38, 
+	.sfp_port[0].pin_los = GPIO_NA, 
+	.sfp_port[0].sds = 1, 
+	.sfp_port[0].i2c =  I2CBUS( GPIO39_I2C_SDA4, GPIO40_I2C_SCL3_MDC1 ),
+
+	// Right SFP port
+	.sfp_port[1].pin_detect = GPIO37,
+	.sfp_port[1].pin_los = GPIO_NA, 
+	.sfp_port[1].sds = 0, 
+	.sfp_port[1].i2c = I2CBUS( GPIO41_I2C_SDA3_MDIO1, GPIO40_I2C_SCL3_MDC1 ),
 
 	.reset_pin = GPIO_NA,
 	.high_leds = { .mux = LED_27 | LED_28_SYS | LED_29, .enable = LED_28_SYS | LED_29 },
@@ -1158,39 +1150,19 @@ __code const struct machine machine = {
 	 * Ports 5-6: Green: 100MBit-10GBit
 	 */
 	.led_sets = { 
-			{
-				LEDS_2G5 | LEDS_LINK | LEDS_ACT,
-				LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK | LEDS_ACT,
-				0,
-				LEDS_2G5 | LEDS_LINK | LEDS_ACT
-			},
-			{  
-				LEDS_10G | LEDS_5G | LEDS_2G5 | LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK | LEDS_ACT,
-				LEDS_10G | LEDS_LINK,
-				0,
-				LEDS_COL | LEDS_DUPLEX
-			},
-			{
-				LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK | LEDS_ACT,
-				LEDS_2G5 | LEDS_1G | LEDS_LINK,
-				LEDS_5G | LEDS_2G5 | LEDS_LINK | LEDS_ACT,
-				LEDS_10G | LEDS_LINK | LEDS_ACT
-			},
-			{
-				LEDS_TX,
-				LEDS_RX,
-				LEDS_10G | LEDS_TWO_PAIR_5G | LEDS_5G | LEDS_TWO_PAIR_2G5 |
-					LEDS_2G5 | LEDS_TWO_PAIR_1G | LEDS_1G | LEDS_500M | LEDS_100M | LEDS_10M | LEDS_ACT,
-				LEDS_10G | LEDS_TWO_PAIR_5G | LEDS_5G | LEDS_TWO_PAIR_2G5 |
-					LEDS_2G5 | LEDS_TWO_PAIR_1G | LEDS_1G | LEDS_500M | LEDS_100M | LEDS_10M | LEDS_LINK
-			},
-	 },
-	.led_mux_custom = 1,
-	.led_mux = {
-        0x04, 0x06, 0x05, 0x08, 0x10, 0x09, 0x0c, 0x3f, 0x0d, 0x10,
-        0x11, 0x0e, 0x14, 0x11, 0x12, 0x15, 0x15, 0x16, 0x18, 0x19,
-        0x1a, 0x19, 0x1d, 0x1e, 0x1c, 0x1d, 0x20, 0x21
+		{ 
+			LEDS_2G5 | LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK | LEDS_ACT, // RJ45 Blue LEDs
+			LEDS_2G5 | LEDS_LINK, // RJ45 White LEDs
+			0,
+			0
 		},
+		{ 
+			LEDS_10G | LEDS_2G5 | LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK | LEDS_ACT, // SFP blue LED?
+			0,
+			0,
+			0
+		},
+	 },
 	};
 
 #elif defined MACHINE_FG_8GT_1SX
